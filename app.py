d0755906fae818b7dc4538d888b0e72f6c1a5e54
Junah Park <junahp@hotmail.com>
2020-08-16 12:57:06 -0700

    reformatted app.py code. attempted fix by changing 'with as' to 'try except', cleaned up code

--- app.py
+++ app.py
@@ -6,30 +6,32 @@ from flask import jsonify
 app = Flask(__name__)
 
 DATABASE = 'database/database.db'
+
 # def get_db():
 #     db = getattr(Flask, '_database', None)
 #     if db is None:
 #         db = Flask._database = sqlite3.connect(DATABASE)
 #     return db
 
+def create_db():
+    c = db_conn().cursor()
+    c.execute("""DROP TABLE IF EXISTS songs;""")
+    table = """CREATE TABLE test(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL
+    );
+    """
+    c.execute(table)
+    c.close()
+    return True
 
-
-
-# def create_db():
-#     with sqlite3.connect(DATABASE) as connection:
-#         c = connection.cursor()
-#         c.execute("""DROP TABLE IF EXISTS songs;""")
-#         table = """CREATE TABLE test(
-#             id INTEGER PRIMARY KEY AUTOINCREMENT,
-#             name TEXT NOT NULL
-#         );
-#         """
-#         c.execute(table)
-#         c.close()
-#     return True
 def db_conn():
-    with sqlite3.connect(DATABASE) as connection:
-        return connection
+    conn = None
+    try:
+        conn = sqlite3.connect(DATABASE)
+    except Error as e:
+        print(e)
+    return conn
 
 @app.route('/')
 def index():
@@ -39,15 +41,14 @@ def index():
 def hello():
     return render_template("public/next.html")
 
-
-
-
 @app.route('/test', methods=['GET','POST'])
 def test():
     data = "failure"
     if request.method == 'GET' or request.method == 'POST':
-        # create_db()
-        db_conn().cursor().execute('INSERT INTO test (name) VALUES("testname")')
+        conn = db_conn()
+        if(conn == None):
+            create_db()
+        conn.cursor().execute('INSERT INTO test (name) VALUES("testname")')
         data="success"
         print("yay")
     return jsonify(data)
