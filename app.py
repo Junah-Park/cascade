32ab43dd430848f2c362291061624f824a4869a2
Junah Park <junahp@hotmail.com>
2020-08-15 21:17:18 -0700

    added db and updated app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
+import sqlite3
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
-
-
+DATABASE = '/path/to/database.db'
 
 @app.route('/')
 def index():
@@ -11,3 +11,16 @@ def index():
 @app.route('/next')
 def hello():
     return render_template("public/next.html")
+
+
+def get_db():
+    db = getattr(Flask, '_database', None)
+    if db is None:
+        db = Flask._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(Flask, '_database', None)
+    if db is not None:
+        db.close()
