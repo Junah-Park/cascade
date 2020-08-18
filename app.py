b99c616a3d8ea00cc0418c16b09c5765bd82e43c
Junah Park <junahp@hotmail.com>
2020-08-18 16:56:22 -0700

    complex

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import sqlite3
 import click
 from flask import Flask, render_template, request, jsonify, redirect, url_for, current_app, g
 from flask.cli import with_appcontext
+from . import db
+
 app = Flask(__name__)
+db.init_app(app)
 
 
 DATABASE = 'database/database.db'
@@ -54,27 +57,18 @@ def init_app(app):
     app.cli.add_command(init_db_command)
 
 
-def create_app():
-    app = ...
-    # existing code omitted
 
-    from . import db
-    db.init_app(app)
-
-    return app
-
-
-def create_db():
-    c = db_conn().cursor()
-    c.execute("""DROP TABLE IF EXISTS songs;""")
-    table = """CREATE TABLE test(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL
-    );
-    """
-    c.execute(table)
-    c.close()
-    return True
+# def create_db():
+#     c = db_conn().cursor()
+#     c.execute("""DROP TABLE IF EXISTS songs;""")
+#     table = """CREATE TABLE test(
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         name TEXT NOT NULL
+#     );
+#     """
+#     c.execute(table)
+#     c.close()
+#     return True
 
 
 def db_conn():
@@ -138,5 +132,64 @@ def close_connection(exception):
         db.close()
 
 
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+    
 if __name__ == "__main__":
     app.run(debug=True)
