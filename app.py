9657292f022b646d87c797c299483f3d5d4be2d3
Kevin Soloway <ksoloway@ucsd.edu>
2020-08-18 18:11:00 -0700

    stuff

--- app.py
+++ app.py
@@ -5,31 +5,31 @@ from flask import render_template
 from flask import jsonify
 app = Flask(__name__)
 
-DATABASE = 'database/database.db'
-# def get_db():
-#     db = getattr(Flask, '_database', None)
-#     if db is None:
-#         db = Flask._database = sqlite3.connect(DATABASE)
-#     return db
+# DATABASE = 'database/database.db'
+# # def get_db():
+# #     db = getattr(Flask, '_database', None)
+# #     if db is None:
+# #         db = Flask._database = sqlite3.connect(DATABASE)
+# #     return db
 
 
 
 
 # def create_db():
 #     with sqlite3.connect(DATABASE) as connection:
-#         c = connection.cursor()
+#         c = connection()
 #         c.execute("""DROP TABLE IF EXISTS songs;""")
 #         table = """CREATE TABLE test(
 #             id INTEGER PRIMARY KEY AUTOINCREMENT,
-#             name TEXT NOT NULL
-#         );
-#         """
+#             name TEXT NOT NULL);"""
 #         c.execute(table)
+#         connection.commit()
 #         c.close()
 #     return True
-def db_conn():
-    with sqlite3.connect(DATABASE) as connection:
-        return connection
+
+# def db_conn():
+#     with sqlite3.connect(DATABASE) as connection:
+#         return connection
 
 @app.route('/')
 def index():
@@ -46,14 +46,14 @@ def hello():
 def test():
     data = "failure"
     if request.method == 'GET' or request.method == 'POST':
-        # create_db()
-        db_conn().cursor().execute('INSERT INTO test (name) VALUES("testname")')
+        # db_conn().execute('''INSERT INTO test(name) VALUES('testname')''')
+        # db_conn().commit()
         data="success"
         print("yay")
     return jsonify(data)
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(Flask, '_database', None)
-    if db is not None:
-        db.close()
+# @app.teardown_appcontext
+# def close_connection(exception):
+#     db = getattr(Flask, '_database', None)
+#     if db is not None:
+#         db.close()
